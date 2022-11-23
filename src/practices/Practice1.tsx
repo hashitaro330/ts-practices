3797ea97511346100c9e684c6fe5be53e68f4f08
hashitaro <117621021+hashitaro330@users.noreply.github.com>
2022-11-23 20:59:59 +0900

    Initial commit

---
+++ src/practices/Practice1.tsx
@@ -0,0 +1,17 @@
+const Practice1 = () => {
+  const calcTotalFee = (num: number) => {
+    const total = num * 1.1;
+    console.log(total);
+  };
+
+  const onClickPractice = () => calcTotalFee(100);
+
+  return (
+    <div>
+      <p>練習問題：引数の型指定</p>
+      <button onClick={onClickPractice}>練習問題１を実行</button>
+    </div>
+  );
+};
+
+export default Practice1;
